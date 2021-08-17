062fdc1480fd7c574d80ea8262cc7e7f04381b68
sergiogutierrez2 <58555535+sergiogutierrez2@users.noreply.github.com>
2021-08-16 22:58:45 -0700

    Uploading C# scripts of Frosty Hikes

---
+++ Unity Scripts/SwitchLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+
+
+            //SceneManager.LoadScene(SceneManager.GetACtiveScene().buildIndex);
+        }
+
+    }
+}
